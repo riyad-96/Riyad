3d2844d4871ee7b52759e265493a8079d31ba3ab
riyad-96 <riyadofficial.ide@gmail.com>
2026-01-21 05:20:30 +0600

    x axis overflow fixed

--- src/components/sections/header/Header.tsx
+++ src/components/sections/header/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed top-0 right-0 left-0 z-50 flex h-17.5 items-center border-b px-4 backdrop-blur-sm transition-colors duration-150 md:px-6 ${
+      className={`sticky top-0 right-0 left-0 z-50 flex h-17.5 items-center border-b px-4 backdrop-blur-sm transition-colors duration-150 md:px-6 ${
         scrolled
           ? 'border-neutral-200 bg-white/90'
           : 'border-transparent bg-white'
@@ -28,25 +28,20 @@ export default function Header() {
         <div className="flex items-center justify-between">
           <button
             onClick={() => scrollToSection('home')}
-            className="relative transition-colors pointer-fine:hover:text-neutral-500 md:text-lg"
+            className="relative transition-colors md:text-lg pointer-fine:hover:text-neutral-500"
           >
             <span className="absolute top-1/2 left-1/2 size-10 -translate-1/2"></span>
             R.
           </button>
+
           <nav className="flex">
             <NavBtn text="about" onClick={() => scrollToSection('about')} />
             <NavBtn
               text="projects"
               onClick={() => scrollToSection('projects')}
             />
-            <NavBtn
-              text="package"
-              onClick={() => scrollToSection('package')}
-            />
-            <NavBtn
-              text="contact"
-              onClick={() => scrollToSection('contact')}
-            />
+            <NavBtn text="package" onClick={() => scrollToSection('package')} />
+            <NavBtn text="contact" onClick={() => scrollToSection('contact')} />
           </nav>
         </div>
       </div>
